9825fea5785d964e87479824f6d642cd4c1d8c81
Matias Borrazas <borrazas.trabajo@gmail.com>
2022-01-20 05:57:08 -0300

    fix

--- views/components/header.php
+++ views/components/header.php
@@ -4,7 +4,6 @@
             <img src="/Logo.jpeg" alt=""  style="float: left;"/>
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
         </button>
         <?php if(isset($_SESSION['user'])): ?>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
